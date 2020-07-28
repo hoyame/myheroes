f32bfff38b2496e660e4ec1cd0cf8aebc873e37a
Hoyame <zou.hoyame@gmail.com>
2020-07-29 01:23:15 +0200

    main: add skeleton

---
+++ MyHeroApp/screens/TabOneScreen.tsx
@@ -0,0 +1,33 @@
+import * as React from 'react';
+import { StyleSheet } from 'react-native';
+
+import EditScreenInfo from '../components/EditScreenInfo';
+import { Text, View } from '../components/Themed';
+
+const TabOneScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Tab One</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    marginTop: -100,
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  separator: {
+    marginVertical: 30,
+    height: 1,
+    width: '80%',
+  },
+});
+
+export default TabOneScreen;
