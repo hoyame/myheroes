22bfecdd7217ea36c45d5a814f5dbd864f1e3175
hoyame <zou.hoyame@gmail.com>
2020-08-28 15:09:17 +0200

    view(home): remake design

--- MyHeroApp/app/views/screens/HomeScreen.tsx
+++ MyHeroApp/app/views/screens/HomeScreen.tsx
@@ -2,11 +2,12 @@ import * as React from 'react';
 import { StyleSheet, Image, TouchableHighlight } from 'react-native';
 import { Text, View } from '../../core/components/Themed';
 import EditScreenInfo from '../../core/components/EditScreenInfo';
-
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
-import { faBars, faAlignLeft } from '@fortawesome/free-solid-svg-icons'
 import HeaderComponent from '../components/Header';
 import HomeButtonComponent from '../components/HomeButtons';
+
+import { faExclamationCircle, faUser, faMapSigns, faSmile } from '@fortawesome/free-solid-svg-icons'
+import { faUserCircle } from '@fortawesome/free-regular-svg-icons'
 
 const styles = StyleSheet.create({
   container: {
@@ -27,8 +28,8 @@ const HomeScreen = () => {
         display: 'flex',
         flexDirection: 'row',
       }}>
-        <HomeButtonComponent color="#47d382" name="Zboub" />
-        <HomeButtonComponent color="#fec11c" name="Zboub" />
+        <HomeButtonComponent fontAwesome={faExclamationCircle} color="#47d382" name="Alertes" fontSize={22.5} />
+        <HomeButtonComponent fontAwesome={faSmile} color="#fec11c" name="Avis" fontSize={22.5} />
       </View>
 
       <View style={{
@@ -36,8 +37,8 @@ const HomeScreen = () => {
         display: 'flex',
         flexDirection: 'row',
       }}>
-        <HomeButtonComponent color="#00cce0" name="Zboub" />
-        <HomeButtonComponent color="#f074b2" name="Zboub" />
+        <HomeButtonComponent fontAwesome={faMapSigns} color="#00cce0" name="Carte" fontSize={22.5} />
+        <HomeButtonComponent fontAwesome={faUserCircle} color="#893ddc" marginTopText={15} name="Mon    Compte" fontSize={22.5} />
       </View>
     </View>
   );
