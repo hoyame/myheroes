976726ab7d9ee7d6659a088a535fdc3d9a5c5c5b
hoyame <zou.hoyame@gmail.com>
2020-08-28 13:42:19 +0200

    component(homepage): end

--- MyHeroApp/app/views/components/HomeButtons/index.tsx
+++ MyHeroApp/app/views/components/HomeButtons/index.tsx
@@ -5,23 +5,44 @@ import EditScreenInfo from '../../../core/components/EditScreenInfo';
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
 import { faBars, faAlignLeft } from '@fortawesome/free-solid-svg-icons'
 
-const HomeButtonComponent = () => {
+interface IHomeButton {
+    color: string;
+    name: string;
+}
+
+const HomeButtonComponent = (props: IHomeButton) => {
     return (
         <TouchableHighlight
         activeOpacity={0.8}
         onPress={() => null}
       >
           <View style={{
-            height: 100,
-            width: 340,
-            backgroundColor: 'red',
-            borderRadius: 7
+            height: 170,
+            width: 130,
+            backgroundColor: props.color,
+            borderRadius: 7,
+            margin: 10
           }}>
-              <Text style={{
-                fontSize: 40
-              }}>
-                Besoin d'aide ?
-              </Text>
+
+            <View style={{
+                height: 60,
+                width: 60,
+                borderRadius: 50,
+                //backgroundColor: '#fff',
+                opacity: 0.40,
+                marginTop: 25,
+                marginLeft: 35,
+            }}>
+
+            </View>
+
+            <Text style={{
+                fontSize: 22.5,
+                marginTop: 30,
+                textAlign: 'center',
+                justifyContent: 'center',
+                alignItems: 'center',
+            }}>{props.name}</Text>
 
           </View>
       </TouchableHighlight>
