6bd5ffa51cb7e167e9796dc70e58c299541e8498
Hoyame Zouhari <56686012+hoyame@users.noreply.github.com>
2020-10-06 21:08:43 +0200

    app(map): follow user position in android

--- MyHeroApp/src/views/Map/map.tsx
+++ MyHeroApp/src/views/Map/map.tsx
@@ -1,12 +1,15 @@
 import { Constants } from "expo";
 import React, { Component } from "react";
-import { Platform, View, Text } from "react-native";
+import { Platform, View, Text, Dimensions } from "react-native";
 import * as Location from 'expo-location';
 import * as Permissions from 'expo-permissions';
+import MapView from "react-native-maps";
 
 export default class MapScreen extends Component {
     state = {
       location: null,
+      latitude: 0,
+      longitude: 0,
       errorMessage: '',
     };
   
@@ -19,27 +22,50 @@ export default class MapScreen extends Component {
       }
   
       let location = await Location.getCurrentPositionAsync({});
+      let latitude = await (await Location.getCurrentPositionAsync({})).coords.latitude;
+      let longitude = await (await Location.getCurrentPositionAsync({})).coords.longitude;
       this.setState({ location });
+      this.setState({ latitude });
+      this.setState({ longitude });
     };
   
     render() {
+        const screenWidth = Math.round(Dimensions.get('window').width);
+        const screenHeight = Math.round(Dimensions.get('window').height);
+
         this._getLocationAsync();
         let text = 'Waiting..';
+        let latitudeS = 0;
+        let longitudeS = 0;
+
         if (this.state.errorMessage) {
           text = this.state.errorMessage;
         } else if (this.state.location) {
           text = JSON.stringify(this.state.location);
+          latitudeS = parseFloat(JSON.stringify(this.state.latitude).replace(/,/g, ''));
+          longitudeS = parseFloat(JSON.stringify(this.state.longitude).replace(/,/g, ''));
         }
       
         return (
-          <View style={{
-              display: "flex",
-              flex: 1,
-              justifyContent: "center",
-              alignItems: "center"
-          }}>
-            <Text>{text}</Text>
-          </View>
+            <>
+              <MapView
+                  showsUserLocation={true}
+
+                  style={{
+                      height: screenHeight,
+                      width: screenWidth,
+                      borderRadius: 10
+                  }}
+
+                  region={{
+                      latitude: latitudeS,
+                      longitude: longitudeS,
+                      latitudeDelta: 0.015,
+                      longitudeDelta: 0.0121,
+                  }}
+                >
+              </MapView>
+            </>
         );
     }
   }
