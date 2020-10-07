158aafdd4d2134d9d744c9d1fd331b766f81f0c1
Hoyame Zouhari <56686012+hoyame@users.noreply.github.com>
2020-10-07 17:35:26 +0200

    feat(home): set location in mini map

---
+++ MyHeroApp/src/views/Map/minimap.tsx
@@ -0,0 +1,71 @@
+import { Constants } from "expo";
+import React, { Component } from "react";
+import { Platform, View, Text, Dimensions } from "react-native";
+import * as Location from 'expo-location';
+import * as Permissions from 'expo-permissions';
+import MapView from "react-native-maps";
+
+export default class MapComponent extends Component {
+    state = {
+      location: null,
+      latitude: 0,
+      longitude: 0,
+      errorMessage: '',
+    };
+  
+    _getLocationAsync = async () => {
+        let { status } = await Permissions.askAsync(Permissions.LOCATION);
+        if (status !== 'granted') {
+          this.setState({
+            errorMessage: 'Permission to access location was denied',
+          });
+        }
+  
+        let location = await Location.getCurrentPositionAsync({});
+        let latitude = await (await Location.getCurrentPositionAsync({})).coords.latitude;
+        let longitude = await (await Location.getCurrentPositionAsync({})).coords.longitude;
+        this.setState({ location });
+        this.setState({ latitude });
+        this.setState({ longitude });
+    };
+  
+    render() {
+        const screenWidth = Math.round(Dimensions.get('window').width - 70);
+
+        this._getLocationAsync();
+        let text = 'Waiting..';
+        let latitudeS = 0;
+        let longitudeS = 0;
+
+        if (this.state.errorMessage) {
+          text = this.state.errorMessage;
+        } else if (this.state.location) {
+          text = JSON.stringify(this.state.location);
+          latitudeS = parseFloat(JSON.stringify(this.state.latitude).replace(/,/g, ''));
+          longitudeS = parseFloat(JSON.stringify(this.state.longitude).replace(/,/g, ''));
+        }
+      
+        return (
+            <>
+              <MapView
+                    showsUserLocation={true}
+		            followsUserLocation={true}
+
+                    style={{
+                        height: 170,
+                        width: screenWidth,
+                        borderRadius: 10
+                    }}
+
+                    region={{
+                        latitude: latitudeS,
+                        longitude: longitudeS,
+                        latitudeDelta: 0.015,
+                        longitudeDelta: 0.0121,
+                    }}
+                >
+              </MapView>
+            </>
+        );
+    }
+  }
