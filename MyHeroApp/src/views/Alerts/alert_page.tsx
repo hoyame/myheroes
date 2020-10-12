366116f934ce6109aef3974ab5395982ba61276b
Hoyame Zouhari <56686012+hoyame@users.noreply.github.com>
2020-10-12 22:22:20 +0200

    app(AlertPage): fix

--- MyHeroApp/src/views/Alerts/alert_page.tsx
+++ MyHeroApp/src/views/Alerts/alert_page.tsx
@@ -5,6 +5,47 @@ import { Dimensions, ScrollView, Text, TouchableHighlight, TouchableOpacity, Vie
 import HeaderComponent from '../../components/Header/header';
 
 const AlertPageScreen = ({ navigation }) => {
+    const AlertInformationProps = () => {
+        return (
+            <View style={{
+                height: 100,
+                borderRadius: 8,
+                display: "flex",
+                flexDirection: 'row',
+                backgroundColor: "#d80000"
+            }}>
+                <View style={{
+                    height: 100,
+                    width: 100,
+                    borderRadius: 100,
+                    opacity: 0.40,
+                    justifyContent: "center",
+                    alignItems: "center"
+                }}>
+                    <FontAwesomeIcon icon={faExclamationCircle} size={55}></FontAwesomeIcon>
+                </View>
+
+                <View style={{
+                    justifyContent: "center",
+                }}>
+                    <Text style={{
+                        fontSize: 25
+                    }}>Grave</Text>
+
+                    <Text style={{
+                        color: "#262626",
+                        fontSize: 15
+                    }}>A 200km sur Chambery</Text>
+
+                    <Text style={{
+                        color: "#262626",
+                        fontSize: 15
+                    }}>De Hoyame</Text>
+                </View>
+            </View>
+        );
+    }
+
     return (
         <>
             <HeaderComponent navigation={navigation} />
@@ -16,9 +57,40 @@ const AlertPageScreen = ({ navigation }) => {
             }}>
                 <Text style={{
                     fontSize: 35,
-                    marginBottom: 20
+                    marginBottom: 5
                 }}>Alerte</Text>
+                
+                <Text style={{
+                    fontSize: 15,
+                    marginBottom: 20
+                }}>ID de l'alerte: 1541161</Text>
 
+                <AlertInformationProps />
+
+                <View style={{
+                    
+                }}>
+
+                </View>
+
+                <View style={{
+                    marginTop: 15
+                }}>
+                    <Text style={{
+                        fontSize: 20,
+                        marginBottom: 10
+                    }}>
+                        Description: 
+                    </Text>
+
+                    <Text style={{
+                        color: "#262626",
+                        fontSize: 15,
+                    }}>
+                        Je me fais suivre par une voiture, depuis 1h, 
+                        ils ont armée et je suis dans la campagne.
+                    </Text>
+                </View>
             </View>
         </>
     );
