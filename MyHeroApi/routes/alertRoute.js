ed79815f2aed5344a385db136ae2f4d318d4f4fd
Hoyame Zouhari <56686012+hoyame@users.noreply.github.com>
2020-10-15 23:03:31 +0200

    api: add alert router

---
+++ MyHeroApi/routes/alertRoute.js
@@ -0,0 +1,11 @@
+const { Router } = require('express');
+const router = Router();
+
+const alertController = require('../controllers/alertController');
+
+router.get('/alerts/get', alertController.returnAlerts);
+
+router.post('/alerts/add', alertController.addAlert);
+
+
+module.exports = router;
