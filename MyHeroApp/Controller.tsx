baad42c7476d5e66583d5455ac9df89d24670dde
Hoyame Zouhari <56686012+hoyame@users.noreply.github.com>
2020-10-20 16:33:06 +0200

    app: add controller

---
+++ MyHeroApp/Controller.tsx
@@ -0,0 +1,111 @@
+import React, { useEffect } from 'react';
+import { StyleSheet, View, Text, Dimensions, Platform } from 'react-native';
+import { View as DefaultView } from 'react-native';
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { Notifications } from 'expo';
+import * as Location from 'expo-location';
+import * as Permissions from 'expo-permissions';
+import * as firebase from 'firebase';
+import { Provider, useDispatch } from 'react-redux';
+
+import HeaderComponent from './src/components/Header/header';
+import NavbarComponent from './src/components/Navbar/navbar';
+import HomeScreen from './src/views/Home/home';
+import AlertScreen from './src/views/Alerts/alerts';
+import NavScreen from './src/views/Nav/nav';
+import MapScreen from './src/views/Map/map';
+import AccountScreen from './src/views/Account/account';
+import ConnexionScreen from './src/views/Connection/connexion';
+import InscriptionScreen from './src/views/Inscription/inscription';
+import AlertPageScreen from './src/views/Alerts/alert_page';
+import CreateAlertScreen from './src/views/Alerts/create_alert';
+import store from './src/data/store';
+import { setLocalisation } from './src/data/actions/localisation';
+import { MyHeroService } from './src/Service';
+
+const styles = StyleSheet.create({
+  flex: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+});
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCH29QA_5aTnTKjEZguPNjtQXfwmwxrbFU",
+  //authDomain: "project-id.firebaseapp.com",
+  databaseURL: "https://myhero-291513.firebaseio.com",
+  projectId: "myhero-291513",
+  storageBucket: "myhero-291513.appspot.com",
+  messagingSenderId: "sender-id",
+  appId: "app-id",
+  measurementId: "G-measurement-id"
+};
+
+
+
+const Controller = () => {
+  const screenWidth = Math.round(Dimensions.get('window').width);
+  const screenHeight = Math.round(Dimensions.get('window').height);
+  const Stack = createStackNavigator();
+  const dispatch = useDispatch();
+
+  let tokenNotifAndroid;
+  let localisationData;
+
+  useEffect(() => {
+    tokenNotifAndroid = MyHeroService.getNotificationToken();
+    localisationData = MyHeroService.getLocalisation();
+
+    if (localisationData.latitude !== undefined && localisationData.longitude !== undefined) {
+        dispatch(setLocalisation({ latitude: localisationData.latitude, longitude: localisationData.longitude, localisation: true, state: true }))
+    }
+  });
+
+  return (
+    <>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Home" screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="Home" component={HomeScreen} options={{ title: '' }} />
+          <Stack.Screen name="Alert" component={AlertScreen} />
+          <Stack.Screen name="AlertPageScreen" component={AlertPageScreen} />
+          <Stack.Screen name="CreateAlertScreen" component={CreateAlertScreen} />
+          <Stack.Screen name="Map" component={MapScreen} />
+          <Stack.Screen name="Account" component={AccountScreen} />
+          <Stack.Screen name="Connexion" component={ConnexionScreen} />
+          <Stack.Screen name="Inscription" component={InscriptionScreen} />
+          <Stack.Screen name="Nav" component={NavScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
+  );
+  
+}
+
+export default Controller;
+
+
+/*
+
+      //dispatch(setLocalisation({ latitude: latitudeS, longitude: longitudeS, localisation: true, state: true }))
+
+
+
+
+  componentDidMount() {
+    if (Platform.OS == "ios") {
+      this._getLocationAsync();
+
+    } else {
+      try {
+        firebase.initializeApp(firebaseConfig);
+        this._getLocationAsync();
+        this.registerForPushNotification();
+      } catch {
+        return null;
+      }
+    }
+  }
+*/
