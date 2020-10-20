16cd296f7b93374f81fdbcfcda28ecae22c76c5a
Hoyame Zouhari <56686012+hoyame@users.noreply.github.com>
2020-10-20 15:01:38 +0200

    app(redux): update

--- MyHeroApp/src/data/actions/localisation.ts
+++ MyHeroApp/src/data/actions/localisation.ts
@@ -6,14 +6,30 @@ interface ISetLocalisation {
     payload: IPosition;
 }
 
+export const setLocalisation = (data: IPosition) => ({
+    type: SET_LOCALISATION,
+    payload: data,
+});
+
 interface IUpdateLocalisation {
     type: typeof UPDATE_LOCALISATION;
     payload: IPosition;
 }
 
+export const updateLocalisation = (data: IPosition) => ({
+    type: UPDATE_LOCALISATION,
+    payload: data,
+});
+
 interface IDeleteLocalisation {
     type: typeof DELETE_LOCALISATION;
     payload: IPosition;
 }
+
+export const deleteLocalisation = (data: IPosition) => ({
+    type: DELETE_LOCALISATION,
+    payload: data,
+});
+
 
 export type ILocalisationAction = ISetLocalisation | IUpdateLocalisation | IDeleteLocalisation;
