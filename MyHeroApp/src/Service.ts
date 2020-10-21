e03a30f48aab44fcc031a448c5744941bc5640aa
Hoyame Zouhari <56686012+hoyame@users.noreply.github.com>
2020-10-21 22:35:14 +0200

    fix #3

--- MyHeroApp/src/Service.ts
+++ MyHeroApp/src/Service.ts
@@ -1,6 +1,7 @@
 import * as Location from 'expo-location';
 import * as Permissions from 'expo-permissions';
 import * as firebase from 'firebase';
+import * as TaskManager from 'expo-task-manager';
 
 import { Notifications } from 'expo';
 
@@ -9,29 +10,36 @@ export abstract class MyHeroService {
     public static latitude = 0;
     public static longitude = 0;
 
+    public static taskManager() {
+      TaskManager.defineTask("getLocalisation", this.getLocalisation)
+    }
+
     public static async getLocalisation() {
-      let { status } = await Permissions.askAsync(Permissions.LOCATION);
+
+      try {
+        let { status } = await Permissions.askAsync(Permissions.LOCATION);
     
-      if (status !== 'granted') {
-        console.log("marche pô")
-        return
-      }
+        if (status !== 'granted') {
+          console.log("marche pô")
+          return
+        }
+        
+        let location = await Location.getCurrentPositionAsync({});
+        let latitudeA = await (await Location.getCurrentPositionAsync({ enableHighAccuracy: false })).coords.latitude;
+        let longitudeA = await (await Location.getCurrentPositionAsync({ enableHighAccuracy: false })).coords.longitude;
       
-      let location = await Location.getCurrentPositionAsync({});
-      let latitudeA = await (await Location.getCurrentPositionAsync({})).coords.latitude;
-      let longitudeA = await (await Location.getCurrentPositionAsync({})).coords.longitude;
-    
-      let latitudeS = parseFloat(JSON.stringify(latitudeA).replace(/,/g, ''));
-      let longitudeS = parseFloat(JSON.stringify(longitudeA).replace(/,/g, ''));
-
-      this.latitude = latitudeS;
-      this.longitude = longitudeS;
-    
-      console.log(this.latitude);
-      console.log(this.longitude);
-
-      //return data;
-
+        let latitudeS = parseFloat(JSON.stringify(latitudeA).replace(/,/g, ''));
+        let longitudeS = parseFloat(JSON.stringify(longitudeA).replace(/,/g, ''));
+  
+        this.latitude = latitudeS;
+        this.longitude = longitudeS;
+      
+        //console.log(this.latitude);
+        //console.log(this.longitude);
+      } catch (err) {
+        console.log("log", err)
+        console.error("log", err)
+      }
     }
 
     public static async getNotificationToken() {
