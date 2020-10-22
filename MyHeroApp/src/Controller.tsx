0d8e21edc4cba9044f0850300d6573a7d8cb1484
Hoyame Zouhari <56686012+hoyame@users.noreply.github.com>
2020-10-23 00:38:48 +0200

    app(android): reset controller

--- MyHeroApp/src/Controller.tsx
+++ MyHeroApp/src/Controller.tsx
@@ -1,16 +1,5 @@
 import React, { useEffect } from 'react';
 import { StyleSheet, View, Text, Dimensions, Platform } from 'react-native';
-import store from '../o-src/data/store';
-import { setLocalisation } from '../o-src/data/actions/localisation';
-import { MyHeroService } from '../o-src/Service';
-
-const styles = StyleSheet.create({
-  flex: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
-  },
-});
 
 const Controller = () => {
   const screenWidth = Math.round(Dimensions.get('window').width);
