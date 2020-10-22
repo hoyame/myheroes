29fab230db9737c4ebc34a682146324df4400b3f
Hoyame Zouhari <56686012+hoyame@users.noreply.github.com>
2020-10-23 00:57:13 +0200

    app(controller): add stack navigator

--- MyHeroApp/src/Controller.tsx
+++ MyHeroApp/src/Controller.tsx
@@ -1,14 +1,31 @@
 import React, { useEffect } from 'react';
 import { StyleSheet, View, Text, Dimensions, Platform } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import 'react-native-gesture-handler';
+
+function HomeScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+    </View>
+  );
+}
 
 const Controller = () => {
   const screenWidth = Math.round(Dimensions.get('window').width);
   const screenHeight = Math.round(Dimensions.get('window').height);
+  const Stack = createStackNavigator();
 
   
   return (
     <>
-      <Text>zfon</Text>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={HomeScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </>
   );
 }
