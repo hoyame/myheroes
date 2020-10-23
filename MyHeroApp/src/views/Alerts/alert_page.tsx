e49b1370a9d4ae2e04dcd1b3a9f620932a8f8e8d
Hoyame Zouhari <56686012+hoyame@users.noreply.github.com>
2020-10-23 18:27:02 +0200

    e

--- MyHeroApp/src/views/Alerts/alert_page.tsx
+++ MyHeroApp/src/views/Alerts/alert_page.tsx
@@ -3,52 +3,13 @@ import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
 import React from 'react';
 import { Dimensions, ScrollView, Text, TouchableHighlight, TouchableOpacity, View } from "react-native";
 import AccountStats from '../../components/AccountStats';
+import AlertInformationProps from '../../components/AlertPropsDetails';
 import HeaderComponent from '../../components/Header/header';
 
 const AlertPageScreen = ({ navigation }) => {
     const screenWidth = Math.round(Dimensions.get('window').width - 70);
 
-    const AlertInformationProps = () => {
-        return (
-            <View style={{
-                height: 100,
-                marginBottom: 10,
-                borderRadius: 8,
-                display: "flex",
-                flexDirection: 'row',
-                backgroundColor: "#d80000"
-            }}>
-                <View style={{
-                    height: 100,
-                    width: 100,
-                    borderRadius: 100,
-                    opacity: 0.40,
-                    justifyContent: "center",
-                    alignItems: "center"
-                }}>
-                    <FontAwesomeIcon icon={faExclamationCircle} size={55}></FontAwesomeIcon>
-                </View>
-
-                <View style={{
-                    justifyContent: "center",
-                }}>
-                    <Text style={{
-                        fontSize: 25
-                    }}>Grave</Text>
-
-                    <Text style={{
-                        color: "#262626",
-                        fontSize: 15
-                    }}>A 200km sur Chambery</Text>
-
-                    <Text style={{
-                        color: "#262626",
-                        fontSize: 15
-                    }}>De Hoyame</Text>
-                </View>
-            </View>
-        );
-    }
+    
 
     return (
         <>
@@ -69,20 +30,20 @@ const AlertPageScreen = ({ navigation }) => {
                     marginBottom: 20
                 }}>ID de l'alerte: 1541161</Text>
 
-                <View style={{
-                    marginBottom: 10,
-                }}>
-                    <AlertInformationProps />
-                </View>
 
                 <View style={{
                     marginBottom: 10,
                 }}>
                     <AccountStats />
                 </View>
+                
+                <View style={{
+                    marginBottom: 10,
+                }}>
+                    <AlertInformationProps level={1} sender="Hoyame" avatar="" distance="510" />
+                </View>
 
                 <View style={{
-                    marginTop: 15,
                     marginBottom: 20
                 }}>
                     <Text style={{
@@ -95,6 +56,7 @@ const AlertPageScreen = ({ navigation }) => {
                     <Text style={{
                         color: "#262626",
                         fontSize: 15,
+                        marginBottom: 10
                     }}>
                         Je me fais suivre par une voiture, depuis 1h, 
                         ils ont armée et je suis dans la campagne.
