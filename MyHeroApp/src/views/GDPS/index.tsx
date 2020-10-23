3bb83f6a977a61390ceca78bcf8f5b763f75b557
Hoyame Zouhari <56686012+hoyame@users.noreply.github.com>
2020-10-24 01:18:04 +0200

    s

---
+++ MyHeroApp/src/views/GDPS/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const GDPSPage = () => {
+    return (
+        <>
+            <Text>GDPSPage</Text>
+        </> 
+    );
+}
+
+export default GDPSPage;
