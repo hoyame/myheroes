d4f79e008c36cb507399294c82dfcdccfe785aff
Hoyame Zouhari <56686012+hoyame@users.noreply.github.com>
2020-10-23 13:34:38 +0200

    app(app): get geolocation

--- MyHeroApp/src/App.tsx
+++ MyHeroApp/src/App.tsx
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Platform, StyleSheet } from 'react-native';;
 import Controller from './Controller';
 import { Provider } from 'react-redux';
 import store from './data/store';
+import { MyHeroService } from './Service';
 
 const App = () => {
+  useEffect(() => {
+    if (Platform.OS == "ios") {
+      setTimeout(() => {
+        //MyHeroService.getLocalisation();
+        MyHeroService.requestLocationPermission();
+      }, 1000)
+      return
+    } else {
+      try {
+        setTimeout(() => {
+          //MyHeroService.getLocalisation();
+          MyHeroService.requestLocationPermission();
+        }, 1000)
+      } catch {
+        return
+      }
+    }
+  })
+
   return (
     <Provider store={store}>
       <Controller />
