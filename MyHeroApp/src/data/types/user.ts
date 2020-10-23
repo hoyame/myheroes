bddcf9ea33614f71fdee937179e89f65c8eb1529
Hoyame Zouhari <56686012+hoyame@users.noreply.github.com>
2020-10-23 22:14:52 +0200

    app(redux): add user

---
+++ MyHeroApp/src/data/types/user.ts
@@ -0,0 +1,52 @@
+import { IAlert } from "./alerts";
+
+export const SET_NAME = "SET_NAME";
+export const SET_RATE = "SET_RATE";
+export const SET_IMAGE = "SET_IMAGE";
+export const SET_XP = "SET_XP";
+
+export const SET_STATUS_SEND = "SET_STATUS";
+export const SET_STATUS_HELP = "SET_STATUS";
+
+export const SET_SEND_ALERT_ID = "SET_SEND_ALERT_ID";
+export const SET_SEND_ALERT_STATUS = "SET_SEND_ALERT_STATUS";
+export const SET_SEND_ALERT_DATA = "SET_SEND_ALERT_DATA";
+
+export const SET_HELP_ALERT_ID = "SET_HELP_ALERT_ID";
+export const SET_HELP_ALERT_STATUS = "SET_HELP_ALERT_STATUS";
+export const SET_HELP_ALERT_DATA = "SET_HELP_ALERT_DATA";
+
+export const SET_CACHE_CREATE_ALERT_LEVEL = "SET_CACHE_CREATE_ALERT_LEVEL";
+export const SET_CACHE_SHOW_ALERT = "SET_CACHE_SHOW_ALERT";
+
+
+export interface IUserSend {
+    id: number;
+    status: boolean;
+    data: IAlert
+}
+
+export interface IUserHelp {
+    id: number;
+    status: boolean;
+    data: IAlert
+}
+
+export interface IUser {
+    name: string;
+    rate: number;
+    image: string;
+    xp: number;
+
+    statusSend: boolean;
+    statusHelp: boolean;
+
+    send: IUserSend,
+
+    help: IUserHelp,
+
+    cache: {
+        createAlertLevel: number;
+        showAlert: IAlert;
+    }
+}
