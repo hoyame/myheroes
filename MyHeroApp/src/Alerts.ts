9f203e8f6f2a8b31a30e910180f3e20e17937e49
Hoyame Zouhari <56686012+hoyame@users.noreply.github.com>
2020-10-23 12:55:12 +0200

    app: disable usless services

---
+++ MyHeroApp/src/Alerts.ts
@@ -0,0 +1,42 @@
+//import Service from "./Service"
+//
+//class IAlerts {
+//    public id: number | undefined;
+//    public source: string | undefined;
+//    public level: number | undefined;
+//    public longitude: number | undefined;
+//    public latitude: number | undefined;
+//    public description: string | undefined;
+//}
+//
+//export default abstract class Alerts {
+//    public static AlertsData = {};
+//
+//
+//    public static SendAlert(data: IAlerts) {
+//        Service.post('api/alert/add', {
+//            source: data.source,
+//            level: data.level,
+//            longitude: data.longitude,
+//            latitude: data.latitude,
+//            description: data.description
+//        });
+//    }
+//
+//    public static DeleteAlert(data: IAlerts) {
+//        Service.post('api/alert/delete', {
+//            id: data.id,
+//            source: data.source,
+//            level: data.level,
+//            longitude: data.longitude,
+//            latitude: data.latitude,
+//            description: data.description
+//        });
+//    }
+//
+//    public static GetAlerts(data: IAlerts) {
+//        this.AlertsData = Service.get('api/alert/get');
+//
+//        return this.AlertsData;
+//    }
+//}
