d26f435e0153e82140e3580346bf7969307ad9c3
Hoyame Zouhari <56686012+hoyame@users.noreply.github.com>
2020-10-23 12:55:19 +0200

    app: add redux

--- MyHeroApp/src/App.tsx
+++ MyHeroApp/src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { Platform, StyleSheet } from 'react-native';;
 import Controller from './Controller';
+import { Provider } from 'react-redux';
+import store from './data/store';
 
 const App = () => {
-
   return (
+    <Provider store={store}>
       <Controller />
+    </Provider>
   );
 }
 
