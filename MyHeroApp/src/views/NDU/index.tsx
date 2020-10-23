c8b77ea6713c8f201c290fd8d94b5f7eda18d46a
Hoyame Zouhari <56686012+hoyame@users.noreply.github.com>
2020-10-24 01:18:08 +0200

    c

---
+++ MyHeroApp/src/views/NDU/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const NDUPage = () => {
+    return (
+        <>
+            <Text>NGUPage</Text>
+        </> 
+    );
+}
+
+export default NDUPage;
