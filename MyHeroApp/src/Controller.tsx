35f2a588669058c13ab677abc6a9ebd8bdc34b39
Hoyame Zouhari <56686012+hoyame@users.noreply.github.com>
2020-10-24 14:11:26 +0200

    app: add loading

--- MyHeroApp/src/Controller.tsx
+++ MyHeroApp/src/Controller.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { Dimensions } from 'react-native';
+import { ActivityIndicator, Dimensions, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { setLocalisation } from './data/actions/localisation';
@@ -22,12 +22,16 @@ import CreateAlertScreen from './views/Alerts/create_alert';
 import { SenderAcceptAlertPage, HelperAcceptAlertPage } from './views/Alerts/accept_alert';
 import GDPSPage from './views/GDPS/index';
 import NDUPage from './views/NDU/index'
+import { useReduxState } from './data/store';
+import { setImage, setName, setRate, setXp } from './data/actions/user';
+
 
 const Controller = () => {
   const screenWidth = Math.round(Dimensions.get('window').width);
   const screenHeight = Math.round(Dimensions.get('window').height);
   const Stack = createStackNavigator();
   const dispatch = useDispatch();
+  const userInformation = useReduxState(state => state.user.name);
 
   useEffect(() => {
     setTimeout(() => {
@@ -35,13 +39,42 @@ const Controller = () => {
             dispatch(setLocalisation({ latitude: MyHeroService.latitude, longitude: MyHeroService.longitude, localisation: true, state: true }))
         }
     }, 5000)
+
+    setTimeout(() => {
+      dispatch(setName('Hoyame'))
+      dispatch(setRate(3))
+      dispatch(setXp(160))
+      dispatch(setImage('https://hoyame.fr/e399d871b6455e3f2a7b0acd8add87c9.png'))
+  }, 2000)
   });
+
+  if (userInformation == "") {
+    return (
+        <View style={{
+            width: screenWidth,
+            display: "flex",
+            flex: 1,
+            justifyContent: "center",
+            alignItems: "center"
+        }}>
+            <Text style={{
+                color: "#6d9bff",
+                fontSize: 30,
+                marginBottom: 30,
+                textAlign: "center"
+            }}>Initialisation de MyHeroServices</Text>
+
+            <ActivityIndicator size="large" color="#6d9bff" />
+        </View>
+      
+    );
+  }
   
   return (
     <>
       <NavigationContainer>
         <Stack.Navigator initialRouteName="Home" screenOptions={{ headerShown: false }}>
-            <Stack.Screen name="Home" component={HomeScreen} options={{ title: '' }} />
+            <Stack.Screen name="Home" component={HomeScreen} />
             <Stack.Screen name="Alert" component={AlertScreen} />
             <Stack.Screen name="AlertPageScreen" component={AlertPageScreen} />
             <Stack.Screen name="CreateAlertScreen" component={CreateAlertScreen} />
