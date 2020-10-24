e13cb32c6509d7f324bcc9d8e1d12a7287b88e56
Hoyame Zouhari <56686012+hoyame@users.noreply.github.com>
2020-10-24 19:24:13 +0200

    app(redux): end alert types

--- MyHeroApp/src/data/types/alerts.ts
+++ MyHeroApp/src/data/types/alerts.ts
@@ -1,12 +1,14 @@
+import { Value } from "react-native-reanimated";
+
 export const ADD_ALERT = "ADD_ALERT";
 export const GET_ALERTS = "GET_ALERTS";
 export const UPDATE_ALERTS = "UPDATE_ALERTS";
 
 export interface IAlert {
     id?: number;
-    level?: number;
-    source?: string;
-    latitude?: number;
-    longitude?: number;
-    description?: string;
+    level: number;
+    source: string;
+    latitude: number;
+    longitude: number;
+    description: string;
 }
