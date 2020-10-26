024d1a0766edd64bc3092e0b6c29747ae8a4f1f9
Hoyame Zouhari <56686012+hoyame@users.noreply.github.com>
2020-10-26 23:50:56 +0100

    app: link connexion api

--- MyHeroApp/src/views/Connection/connexion.tsx
+++ MyHeroApp/src/views/Connection/connexion.tsx
@@ -1,9 +1,11 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet, Image, Dimensions, Text, TouchableHighlight, TouchableOpacity, View, TextInput } from "react-native";
 import HeaderComponent from '../../components/Header/header';
 import InputComponent from '../../components/Input/input';
 import { faArrowAltCircleLeft, faEnvelope, faLock } from "@fortawesome/free-solid-svg-icons";
 import Users from '../../User';
+import { WaveIndicator } from 'react-native-indicators';
+import AsyncStorage from '@react-native-community/async-storage';
 
 const screenWidth = Math.round(Dimensions.get('window').width - 70);
 
@@ -26,18 +28,82 @@ const STYLES = StyleSheet.create({
   
 
 const ConnexionScreen = ({ navigation }) => {
+    const [status, setStatus] = useState(false)
+    const [error, setError] = useState(false)
+
     const [state, setState] = useState({
         name: '',
         mail: '',
         password: '',
     })
 
+    const _storeData = async () => {
+        try {
+            await AsyncStorage.setItem('@name', state.name)
+            await AsyncStorage.setItem('@mail', state.mail)
+        } catch (error) {
+            console.log("error", error)
+        }
+    };
+
+    //useEffect(() => {
+    //    setStatus(false);
+    //})
+
     const onPress = () => {
+        setStatus(true);
+
         Users.Login({
             pseudo: state.mail,
             email: state.mail,
             password: state.password,
+        }, (res: any) => {
+            if (res == 200) {
+                setTimeout(() => {
+                    _storeData();
+                    navigation.navigate('Home');
+                }, 2500)
+            } else {
+                setTimeout(() => {
+                    setStatus(false);
+                    setError(true);
+                }, 3000)
+            }
         })
+    }
+
+    const Ze = () => {
+        return (
+            <>
+                <Text style={{
+                    color: "#6d9bff",
+                    fontSize: 30,                    
+                    marginBottom: 10,
+                    textAlign: "center"
+                }}>Bienvenue sur MyHero</Text>
+      
+                <Text style={{
+                    color: "#6d9bff",
+                    fontSize: 25,
+                    marginBottom: 30,
+                    textAlign: "center"
+                }}>Connexion en cours</Text>
+            </>
+        );
+    }
+
+    if (status == true) {
+        return (
+            <View style={{
+                display: "flex",
+                flex: 1,
+                justifyContent: "center",
+                alignItems: "center"
+            }}>
+                <Ze />
+                <WaveIndicator color='#6d9bff' size={40} />
+            </View>
+        );
     }
 
     return (
@@ -49,6 +115,7 @@ const ConnexionScreen = ({ navigation }) => {
                 justifyContent: 'center',
                 alignItems: 'center'
             }}>
+                
                 <Text style={{
                     fontSize: 35,
                     textAlign: 'center',
@@ -70,7 +137,7 @@ const ConnexionScreen = ({ navigation }) => {
                     <View style={{
                         display: 'flex',
                         justifyContent: 'center',
-                        marginBottom: 15,
+                        marginBottom: 7.5,
                         flexDirection: 'row'
                     }}>
                         <Text>Vous n'avez pas de compte ?</Text>
@@ -80,6 +147,8 @@ const ConnexionScreen = ({ navigation }) => {
                             }}> S'inscrire</Text>
                         </TouchableOpacity>
                     </View>
+
+                    {error && <Text style={{color: 'red', textAlign: "center", marginBottom: 10}}>Identifiant ou/et mot de passe erronée</Text>}
 
                     <TouchableOpacity onPress={() => onPress()}>
                         <View style={{
