e518349e2b47a2ad538a5787c9769e6c082ffa96
Hoyame Zouhari <56686012+hoyame@users.noreply.github.com>
2020-10-27 19:33:55 +0100

    api: add controller user image

--- MyHeroApi/controllers/usersController.js
+++ MyHeroApi/controllers/usersController.js
@@ -272,3 +272,13 @@ module.exports.resetPassword = (req, res, next) => {
 		return next(err);
 	}
 };
+
+
+module.exports.uploadAvatar = (req, res, next) => {
+	console.log('file', req.files)
+	console.log('body', req.body)
+
+	res.status(200).json({
+	  message: 'success!',
+	})
+}
