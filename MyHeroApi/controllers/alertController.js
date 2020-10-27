d3f61aced49453296abeb38dd582921697e3602b
Hoyame Zouhari <56686012+hoyame@users.noreply.github.com>
2020-10-27 15:41:15 +0100

    api: add remove post

--- MyHeroApi/controllers/alertController.js
+++ MyHeroApi/controllers/alertController.js
@@ -16,8 +16,22 @@ module.exports.addAlert = (req, res, next) => {
     res.send(AlertsData);
 }
 
-module.exports.returnAlerts = (req, res, next) => {
+module.exports.removeAlert = (req, res, next) => {
+    const model = {
+        id: AlertsData.length + 1,
+        level: req.body.level,
+        source: req.body.source,
+        latitude: req.body.latitude,
+        longitude: req.body.longitude,
+        description: req.body.description
+    }
 
+    AlertsData = AlertsData.filter(x => x.source != req.body.source);
+
+    res.send(AlertsData);
+}
+
+module.exports.returnAlerts = (req, res, next) => {
     res.send(AlertsData);
 }
 
