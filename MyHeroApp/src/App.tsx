4703dacf57580ef9d2b27d58c26caacc39b633b4
Hoyame Zouhari <56686012+hoyame@users.noreply.github.com>
2020-10-29 17:34:28 +0100

    app: add axios init

--- MyHeroApp/src/App.tsx
+++ MyHeroApp/src/App.tsx
@@ -3,19 +3,21 @@ import { Platform, StyleSheet } from 'react-native';;
 import Controller from './Controller';
 import { Provider } from 'react-redux';
 import store from './data/store';
-import { MyHeroService } from './Service';
+import { MyHeroService } from './api/Service';
 
 const App = () => {
   useEffect(() => {
     if (Platform.OS == "ios") {
       setTimeout(() => {
         MyHeroService.requestLocationPermission();
+        MyHeroService.initialize();
       }, 1000)
       return
     } else {
       try {
         setTimeout(() => {
           MyHeroService.requestLocationPermission();
+          MyHeroService.initialize();
         }, 1000)
       } catch {
         return
