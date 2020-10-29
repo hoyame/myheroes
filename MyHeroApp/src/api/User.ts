dfef8669991eb7cd20e8348d42b7049c3b273cac
Hoyame Zouhari <56686012+hoyame@users.noreply.github.com>
2020-10-30 00:06:12 +0100

    app(serice): add get data user

--- MyHeroApp/src/api/User.ts
+++ MyHeroApp/src/api/User.ts
@@ -71,69 +71,37 @@ export default abstract class Users {
     }
 
     public static GetData(data: string, cb: any, error: any) {
-        //var myHeaders = new Headers();
-
-        //return fetch(`http://146.59.227.90:3333/user/get?mail=${data}`, { 
-        //    method: 'GET',
-        //    //headers: myHeaders,
-        //    //mode: 'cors',
-        //})
-
         axios.get(`http://146.59.227.90:3333/user/get?mail=${data}`)
             .then((response) => {
-                //console.log(response.data);
-
                 cb(response)
-                //console.log(response.status);
-                //console.log(response.statusText);
-                //console.log(response.headers);
-                //console.log(response.config);
             })
 
             .catch((err) => {
-                console.log("err", err)
-
                 if (err = "[Error: Request failed with status code 500]") {
                    error(500);
                 }
-                
-            })
-
-
-        //const resJson = yield MyHeroService.get(`user/get?mail=${data}`);
-        //const Data = resJson.data;
-
-        //console.log(Data)
-
-
-    
-        //.then((resp) => console.log("gcdytcc", resp)) 
-        //
-        //.then(function(data) {})
-
-        //.catch(function(err) {
-        //    console.log("error", err)  
-        //})
+            }
+        )
     } 
 
-    //public static setAvatar(data: any, cb: any) {
-    //    fetch("http://localhost:3000/api/upload", {
-    //        method: "POST",
-    //        body: JSON.stringify({
-    //            name: data.name,
-    //            type: data.type,
-    //            uri: data.uri
-    //        })
-    //    })
-    //    .then(response => response.json())
-    //    .then(response => {
-    //        console.log("upload succes", response);
+    public static setAvatar(data: any, cb: any) {
+        fetch("http://localhost:3000/api/upload", {
+            method: "POST",
+            body: JSON.stringify({
+                name: data.name,
+                type: data.type,
+                uri: data.uri
+            })
+        })
+        
+        .then(response => response.json())
 
-    //        
-    //    })
-    //    .catch(error => {
-    //        console.log("upload error", error);
+        .then(response => {
+            console.log("upload succes", response);
+        })
 
-    //    });
-    //}
+        .catch(error => {
+            console.log("upload error", error);
+        });
+    }
 }
