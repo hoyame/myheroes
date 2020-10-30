be80751df215ea42e7fad4a17f859eb22a5cee70
Hoyame Zouhari <56686012+hoyame@users.noreply.github.com>
2020-10-30 01:23:33 +0100

    api: fix

--- MyHeroApi/app.js
+++ MyHeroApi/app.js
@@ -60,7 +60,7 @@ app.use((err, req, res, next) => {
 });
 
 // Run the server
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3333;
 app.listen(port, () =>
 	console.log(`🐹 app listening on http://localhost:${port}`)
 );
