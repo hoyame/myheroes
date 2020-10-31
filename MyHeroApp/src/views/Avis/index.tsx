e3d484a54c7f3f1a1a97bd27353a3d7116ef0e74
Hoyame Zouhari <56686012+hoyame@users.noreply.github.com>
2020-10-31 21:32:58 +0100

    app: add avis page

--- MyHeroApp/src/views/Avis/index.tsx
+++ MyHeroApp/src/views/Avis/index.tsx
@@ -1,15 +1,25 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 import HeaderComponent from '../../components/Header/header';
+import RateComponent from '../../components/Rate';
 
 const AvisScreen = ({ navigation }) => {
     return (
         <>
             <HeaderComponent navigation={navigation} />
 
-            <View>
-                <Text>Avis</Text>
+            <View style={{
+                display: "flex", 
+                justifyContent: "center",
+                alignItems: "center"
+            }}>
+                <Text style={{
+                    fontSize: 25,
+                    textAlign: "center",
+                    marginBottom: 5
+                }}>MyHeroes</Text>
 
+                <RateComponent title="Noter l'application" placeholder="Avis" onClick={() => null} />
             </View>
         </>
     );
