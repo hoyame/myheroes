1b9a0bf98a67c2b5145db7aed58b1a8c8c5f3104
Hoyame Zouhari <56686012+hoyame@users.noreply.github.com>
2020-11-03 16:52:25 +0100

    fix

--- MyHeroApp/src/api/Service.ts
+++ MyHeroApp/src/api/Service.ts
@@ -6,6 +6,12 @@ const apiLink = 'http://146.59.227.90:3333/';
 axios.defaults.baseURL = apiLink;
 axios.defaults.timeout = 1000 * 60 * 5;
 
+interface IOptions {
+  method?: any;
+  headers?: any;
+  body?: any;
+}
+
 export abstract class MyHeroService {
     public static notInitialized: boolean = true;
     public static instance: AxiosInstance;
@@ -87,4 +93,18 @@ export abstract class MyHeroService {
 
     }
 
+    public static futch(url: string, opts: IOptions, onProgress: ((this: XMLHttpRequest, ev: ProgressEvent) => any) | null) {
+      console.log(url, opts)
+      return new Promise( (res, rej)=>{
+          var xhr = new XMLHttpRequest();
+          xhr.open(opts.method || 'get', url);
+          for (var k in opts.headers||{})
+              xhr.setRequestHeader(k, opts.headers[k]);
+          xhr.onload = (e: any) => res(e.target);
+          xhr.onerror = rej;
+          if (xhr.upload && onProgress)
+              xhr.upload.onprogress = onProgress; // event.loaded / event.total * 100 ; //event.lengthComputable
+          xhr.send(opts.body);
+      });
+  }
 }
