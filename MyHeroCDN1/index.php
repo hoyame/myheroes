1d8510a8f3845ff57a7b5768d54a0b085426caee
Hoyame Zouhari <56686012+hoyame@users.noreply.github.com>
2020-11-03 17:16:18 +0100

    cdn1: update

--- MyHeroCDN1/index.php
+++ MyHeroCDN1/index.php
@@ -2,7 +2,24 @@
 
 $data = array('status' => false);
 
-header('Acccess-Control-Allow-Origin: *');
-header('Content=type:application/json');
+if(isset($_POST['sumbit'])){
+    $target_file = basename($_FILES['file']['name']);
+    $file_type = pathinfo($target_file, PATHINFO_EXTENSION);
+    $is_image = getimagessize($_FILES['file']['tmp_name']);
+
+    if($is_image){
+        $data['image'] = time() . '.' . $file_type;
+        if(move_uploaded_file($_FILES['file']['tmp_name'], $data['image'])){*
+            $data['status'] = true;
+        } else {
+            $data['message'] = 'error on uploading image'
+        }
+    } else {
+        $data['message'] = 'file is not an image'
+    }
+}
+
+header('Access-Control-Allow-Origin: *');
+header('Content-type:application/json');
 
 echo json_encode($data);
