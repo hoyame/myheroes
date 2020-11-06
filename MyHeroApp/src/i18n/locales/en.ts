9d4c7bba6a6c29c4184266f1e088f3c64e532454
Hoyame Zouhari <56686012+hoyame@users.noreply.github.com>
2020-11-06 01:28:39 +0100

    app: add translate support in nav & home

--- MyHeroApp/src/i18n/locales/en.ts
+++ MyHeroApp/src/i18n/locales/en.ts
@@ -4,10 +4,36 @@ export default {
     alertGrave: "Grave",
     alertMoyen: "Moyen",
     alertFaible: "Faible",
-
     alertDescGrave: "(Accident, agression, malaise...)",
     alertDescMoyen: "(En panne, coincé, vol...)",
     alertDescFaible: "(Perdu, nuisance, autre...)",
-  
     alertInProgress: "Alerte en cours",
+    alertInProgressDesc: "Page de l'alerte",
+
+    menu: "Menu",
+
+    acceuil: "Acceuil",
+    carte: "Carte",
+    alertes: "Alertes",
+    avis: "Avis",
+    parametres: "Parametres",
+    account: "Account",
+    faq: "F.A.Q",
+    confidentiality: "Confidentialité",
+    apropos: "A propos",
+    gdps: "Gestes de premier secours",
+    ndu: "Numeros d'urgence",
+
+    descacceuil: "Page d'acceuil",
+    desccarte: "Carte avec les alertes",
+    descalertes: "Listes des alertes",
+    descavis: "Votre avis sur l'application",
+    descparametres: "Parametres du compte",
+    descaccount: "Mon compte",
+    descfaq: "Foire aux questions",
+    descconfidentiality: "Confidentialité",
+    descapropos: "A propos",
+    descgdps: "Gestes de premier secours",
+    descndu: "Numeros d'urgence",
+
 };
