ce95d846032814f97728c1f65fd2a1a5a0f45459
Hoyame Zouhari <56686012+hoyame@users.noreply.github.com>
2020-11-06 01:15:02 +0100

    app: init trad

--- MyHeroApp/src/i18n/locales/fr.ts
+++ MyHeroApp/src/i18n/locales/fr.ts
@@ -1,3 +1,11 @@
 export default {
     hello: "Hello",
+
+    alertGrave: "Grave",
+    alertMoyen: "Moyen",
+    alertFaible: "Faible",
+
+    alertDescGrave: "(Accident, agression, malaise...)",
+    alertDescMoyen: "(En panne, coincé, vol...)",
+    alertDescFaible: "(Perdu, nuisance, autre...)",
 };
