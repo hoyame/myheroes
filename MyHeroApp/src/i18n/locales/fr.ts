fcdd4d13b653e2ae2a00de425c3c3a282d662375
Hoyame Zouhari <56686012+hoyame@users.noreply.github.com>
2020-11-06 20:12:05 +0100

    app: translate update

--- MyHeroApp/src/i18n/locales/fr.ts
+++ MyHeroApp/src/i18n/locales/fr.ts
@@ -1,11 +1,48 @@
 export default {
     hello: "Hello",
 
+    errorServMH: "Les serveurs MyHeroes sont actuellement indisponibles",
+    initMHServ: "Initialisation de",
+
     alertGrave: "Grave",
     alertMoyen: "Moyen",
     alertFaible: "Faible",
-
     alertDescGrave: "(Accident, agression, malaise...)",
     alertDescMoyen: "(En panne, coincé, vol...)",
     alertDescFaible: "(Perdu, nuisance, autre...)",
+    alertInProgress: "Alerte en cours",
+    alertInProgressDesc: "Page de l'alerte",
+
+    menu: "Menu",
+
+    acceuil: "Acceuil",
+    carte: "Carte",
+    alertes: "Alertes",
+    avis: "Avis",
+    parametres: "Parametres",
+    account: "Mon compte",
+    faq: "F.A.Q",
+    confidentiality: "Confidentialité",
+    apropos: "A propos",
+    gdps: "Gestes de premier secours",
+    ndu: "Numeros d'urgence",
+
+    descacceuil: "Page d'acceuil",
+    desccarte: "Carte avec les alertes",
+    descalertes: "Listes des alertes",
+    descavis: "Votre avis sur l'application",
+    descparametres: "Parametres du compte",
+    descaccount: "Mon compte",
+    descfaq: "Foire aux questions",
+    descconfidentiality: "Confidentialité",
+    descapropos: "A propos",
+    descgdps: "Gestes de premier secours",
+    descndu: "Numeros d'urgence",
+
+    accountTrophes: "Trophées",
+    accountAvis: "Avis",
+
+    avisNotDispo: "Aucune alerte disponible",
+    avisReturnAcceuil: "Retour a l'acceuil",
+    avisActivCam: "Vous voulez activer votre camera ?",
 };
