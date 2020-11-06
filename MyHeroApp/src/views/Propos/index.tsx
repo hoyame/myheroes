55bb0cb2bbec8475671ce793192ab67fe5531115
Hoyame Zouhari <56686012+hoyame@users.noreply.github.com>
2020-11-06 20:27:07 +0100

    app: translate update

--- MyHeroApp/src/views/Propos/index.tsx
+++ MyHeroApp/src/views/Propos/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 import HeaderComponent from '../../components/Header/header';
+import I18n from '../../i18n/i18n';
 
 const ProposScreen = ({ navigation }) => {
     return (
