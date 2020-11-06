5d90b243c4962b9d32e858a297f36ee4dfaf6c66
Hoyame Zouhari <56686012+hoyame@users.noreply.github.com>
2020-11-06 20:26:44 +0100

    app: translate update

--- MyHeroApp/src/views/Avis/index.tsx
+++ MyHeroApp/src/views/Avis/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { View, Text } from 'react-native';
 import HeaderComponent from '../../components/Header/header';
 import RateComponent from '../../components/Rate';
+import I18n from '../../i18n/i18n';
 
 const AvisScreen = ({ navigation }) => {
     return (
@@ -19,7 +20,7 @@ const AvisScreen = ({ navigation }) => {
                     marginBottom: 5
                 }}>MyHeroes</Text>
 
-                <RateComponent title="Noter l'application" placeholder="Avis" onClick={() => null} />
+                <RateComponent title={I18n.t("avisRateApp")} placeholder={I18n.t("avisDescApp")} onClick={() => null} />
             </View>
         </>
     );
