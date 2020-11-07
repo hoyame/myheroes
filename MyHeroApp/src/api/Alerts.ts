35de353a45aef88e9270027ad2fc18609dfb2159
Hoyame Zouhari <56686012+hoyame@users.noreply.github.com>
2020-11-07 19:23:43 +0100

    app: fix ?

--- MyHeroApp/src/api/Alerts.ts
+++ MyHeroApp/src/api/Alerts.ts
@@ -4,6 +4,23 @@ import { MyHeroService } from "./Service";
 
 var myHeaders = new Headers();
 
+function getDistanceFromLatLonInKm(lat1: number, lon1: number, lat2: number, lon2: number) {
+    var R = 6371; // Radius of the earth in km
+    var dLat = deg2rad(lat2-lat1);  // deg2rad below
+    var dLon = deg2rad(lon2-lon1); 
+    var a = 
+      Math.sin(dLat/2) * Math.sin(dLat/2) +
+      Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * 
+      Math.sin(dLon/2) * Math.sin(dLon/2)
+      ; 
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+    var d = R * c; // Distance in km
+    return d;
+}
+  
+function deg2rad(deg: number) {
+  return deg * (Math.PI/180)
+}
 
 const fetchApiCall = async () => {
     var params = {
@@ -77,19 +94,6 @@ export default abstract class MyHeroAlerts {
     public static GetAlerts() {
         this.StatusUpdate = true;
 
-        fetch(`${API_LINK}/alerts/remove`, { 
-            method: 'GET',
-            headers: myHeaders,
-            mode: 'cors',
-        })
-            .then(function(res) {
-                //console.log(res);
-            })
-
-            .catch(function(err) {
-                //console.log("errror", err)
-            })
-        ;
-            return this.AlertsData;
-        }
+        
+    }
 }
