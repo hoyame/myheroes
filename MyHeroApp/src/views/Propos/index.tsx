b7ba7710de62aaf48bb327ed15dc968309d92e74
Hoyame Zouhari <56686012+hoyame@users.noreply.github.com>
2020-11-08 14:11:39 +0100

    feat: update

--- MyHeroApp/src/views/Propos/index.tsx
+++ MyHeroApp/src/views/Propos/index.tsx
@@ -6,7 +6,7 @@ import I18n from '../../i18n/i18n';
 const ProposScreen = ({ navigation }) => {
     return (
         <>
-            <HeaderComponent navigation={navigation} />
+            <HeaderComponent title={I18n.t("apropos")} navigation={navigation} />
 
             <View>
                 <Text>{I18n.t("apropos")}</Text>
