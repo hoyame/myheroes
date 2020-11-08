5d3ebbc6135f783b90757c35021a2c1610c0af96
Hoyame Zouhari <56686012+hoyame@users.noreply.github.com>
2020-11-08 14:01:46 +0100

    up

--- MyHeroApp/src/components/Header/header.tsx
+++ MyHeroApp/src/components/Header/header.tsx
@@ -23,7 +23,23 @@ const HeaderComponent = (props: IHeader) => {
     
     return (
         <>
-            <View style={{ padding: 35}}>
+            <View style={ 
+                Dimensions.get('window').height > 695 ? {
+                    marginTop: 50,
+                    display: "flex", 
+                    alignItems: "center",
+                    paddingLeft: 35,
+                    paddingRight: 35,
+                    paddingBottom: 35,
+                } : {
+                    marginTop: 20,
+                    display: "flex", 
+                    alignItems: "center",
+                    paddingLeft: 35,
+                    paddingRight: 35,
+                    paddingBottom: 30               
+                }
+            }>
                 <View style={{alignItems: "center"}}>
                     <View style={{
                         marginBottom: 25,
