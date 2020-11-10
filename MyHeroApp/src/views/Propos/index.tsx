af8a3332cc3d29a34aa75f481ba5643a2f305e82
Hoyame Zouhari <56686012+hoyame@users.noreply.github.com>
2020-11-10 21:10:10 +0100

    app(propos): add translate support

--- MyHeroApp/src/views/Propos/index.tsx
+++ MyHeroApp/src/views/Propos/index.tsx
@@ -16,53 +16,36 @@ const ProposScreen = ({ navigation }) => {
                     <Text style={{
                         fontSize: 25,
                         marginBottom: 10
-                    }}>Historique</Text>
+                    }}>{I18n.t("faq1")}</Text>
     
                     <Text style={{
                         fontSize: 18,
                         marginBottom: 15
-                    }}>Crée en 2019 par SAIDAT Wahib, l'application MyHeroes de
-                        a pour but de sauver la population quelque soit leur situation.
-                        L'equipe composé du directeur SAIDAT Wahib et de son développeur 
-                        Hoyame Zouhari ont décidé d'apporter une assistance rapide suite a
-                        tout les mouvements mauvais qu'il y'a eu ( terrorisme, metou, aggressions scolaires
-                        , etc...)
-                    </Text>
+                    }}>{I18n.t("faq1Desc")}</Text>
                         
+                    {/* */}
+
                     <Text style={{
                         fontSize: 25,
                         marginBottom: 10
-                    }}>Objectif</Text>
+                    }}>{I18n.t("faq2")}</Text>
     
                     <Text style={{
                         fontSize: 18,
                         marginBottom: 15
-                    }}>
-                        L'application MyHeroes a pour obkectif d'aider la population le plus
-                        rapidement possible quelque coit leur situation qu'elle soit la gravité
-                        qu'elle encours. Grace a MyHeroes, que MyHero peut arriver sur place sur place
-                        en quelque secondes tandis que les secours sont la en quelques minutes.
-                        Parfois il suffit de quelques secondes pour sauver une vie ou empecher une aggression
-                        et MyHeroes est la pour ça.
-                    </Text>
+                    }}>{I18n.t("faq2Desc")}</Text>
                 
+                    {/* */}
+
                     <Text style={{
                         fontSize: 25,
                         marginBottom: 10
-                    }}>Services</Text>
+                    }}>{I18n.t("faq3")}</Text>
     
                     <Text style={{
                         fontSize: 18,
                         marginBottom: 15
-                    }}>
-                        L'application MyHeroes propose que toute les personnes s'entraide les unes aux autres grace 
-                        a une geolocalisation qui permet de voir les perosnnes en danger a proximité. MyHeroes apprend
-                        aux gens comment se comporter en cas de grave danger grace aux gestes de premier secours et qui 
-                        appeler au plus vite grace aux numeros d'urgence. MyHeroes permet de gagner des points pour chaque 
-                        sauvetage, plus vous avez de point, plus vous montez de niveau et plus vous êtes un grand/grande heros/heroine
-                        pour la nation
-                        
-                    </Text>
+                    }}>{I18n.t("faq3Desc")}</Text>
                 </View>
             </ScrollView>
         </>
