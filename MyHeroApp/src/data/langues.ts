68ae84b62dff7069ed6f84089a9f5e95ecd0e2ad
Hoyame Zouhari <56686012+hoyame@users.noreply.github.com>
2020-11-11 18:56:07 +0100

    ez

---
+++ MyHeroApp/src/data/langues.ts
@@ -0,0 +1,4 @@
+export const Langues = [
+    { name: "Français", id: "fr", img: "https://flagcdn.com/80x60/za.png" },
+    { name: "Italiano", id: "it", img: "https://flagcdn.com/80x60/it.png" }
+]
