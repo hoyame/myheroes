4777426c65a2c90ff92124d796ef640118e3965f
Hoyame Zouhari <56686012+hoyame@users.noreply.github.com>
2020-11-13 00:55:50 +0100

    app(general): init

--- MyHeroApp/src/views/General/index.tsx
+++ MyHeroApp/src/views/General/index.tsx
@@ -3,16 +3,16 @@ import { View, Text } from 'react-native';
 import HeaderComponent from '../../components/Header/header';
 import I18n from '../../i18n/i18n';
 
-const ConfidentialiteScreen = ({ navigation }) => {
+const GeneralScreen = ({ navigation }) => {
     return (
         <>
-            <HeaderComponent title={I18n.t("confidentiality")} navigation={navigation} />
+            <HeaderComponent title={I18n.t("acceuil")} navigation={navigation} />
 
             <View>
-                <Text>{I18n.t("confidentiality")}</Text>
+                <Text>{I18n.t("acceuil")}</Text>
             </View>
         </>
     );
 }
 
-export default ConfidentialiteScreen;
+export default GeneralScreen;
