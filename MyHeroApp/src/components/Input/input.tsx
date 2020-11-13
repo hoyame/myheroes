5f15d2746925b73bea050489372bc7eda2fe1b26
Hoyame Zouhari <56686012+hoyame@users.noreply.github.com>
2020-11-13 18:47:38 +0100

    app: update style input

--- MyHeroApp/src/components/Input/input.tsx
+++ MyHeroApp/src/components/Input/input.tsx
@@ -30,11 +30,15 @@ const InputComponent = (props: IInput) => {
             flexDirection: "row",
             height: height, 
             width: screenWidth,
-            borderColor: 'gray', 
-            borderWidth: 1,
-            borderRadius: 10,
-            marginBottom: 15,
-            //paddingLeft: 20
+            backgroundColor:"white",
+            borderRadius:15,
+            padding:10,
+            elevation:10,
+            shadowColor: '#000',
+            shadowOffset: { width: 0, height: 3 },
+            shadowOpacity: 0.5,
+            shadowRadius: 5, 
+            marginBottom: 15
         },
     
         ICON: {
@@ -45,60 +49,18 @@ const InputComponent = (props: IInput) => {
     return (
         <>
             <View style={STYLES.INPUT}>
-                <View style={{
-                    height: 65, 
-                    width: 65, 
-                    borderRadius: 10, 
-                    justifyContent: "center",
-                    alignItems: "center"
-                }}>
-                    <FontAwesomeIcon icon={props.icon} size={25} style={STYLES.ICON} />
-                </View>
-
-                <View style={{
-                    //justifyContent: "center",
-                }}>
-                    <Text style={ Platform.OS == "android" ? { 
-                        color: "#6d9bff",
-                        marginLeft: 5,
-                        marginTop: 3,
-                        marginBottom: -8
-                    } : { 
-                        color: "#6d9bff",
-                        marginLeft: 1,
-                        marginTop: 3,
-                        marginBottom: 1
-                    }}>{props.name}</Text>
-    
-                    <TextInput
-                        style={{
-                            fontSize: 20
-                        }}
-                        placeholder={props.placeholder}
-                        onChangeText={text => props.onChange(text)}
-                        value={props.value}
-                        secureTextEntry={props.password == true ? true : false }
-                    />
-                </View>
+                <TextInput
+                    style={{
+                        fontSize: 17.5
+                    }}
+                    placeholder={props.placeholder}
+                    onChangeText={text => props.onChange(text)}
+                    value={props.value}
+                    secureTextEntry={props.password == true ? true : false }
+                />
             </View>
         </>
     );
 }
 
 export default InputComponent;
-
-/*
-setState({...state, name: text})
-
-                <Text style={{
-                        fontSize: 20
-                    }}>eefgniuegib@gmzf.com</Text>
-
-<TextInput
-    style={STYLES.INPUT}
-    placeholder="Pseudo"
-    onChangeText={text => setState({...state, name: text})}
-    value={state.name}
-/>
-
-*/
