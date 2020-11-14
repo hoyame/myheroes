9ddbf0a08110996a8907b469327ff3f8a1a3abe4
Hoyame Zouhari <56686012+hoyame@users.noreply.github.com>
2020-11-14 14:58:46 +0100

    app(component): add fond

---
+++ MyHeroApp/src/components/Fond/index.tsx
@@ -0,0 +1,47 @@
+import { faCircle } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const FondComponent = () => {
+    return (
+        <>
+            <FontAwesomeIcon icon={faCircle} size={240} style={{
+                    position: 'absolute',
+                    top: 150,
+                    right: -150,
+                    color: "#008B00"
+            }} />
+            
+            <FontAwesomeIcon icon={faCircle} size={150} style={{
+                position: 'absolute',
+                top: 260,
+                left: -50,
+                color: "#E63B25"
+            }} />
+
+            <FontAwesomeIcon icon={faCircle} size={120} style={{
+                position: 'absolute',
+                bottom: -60,
+                right: -50,
+                color: "#1F7CEB"
+            }} />
+
+            <FontAwesomeIcon icon={faCircle} size={120} style={{
+                position: 'absolute',
+                top: -60,
+                left: -50,
+                color: "#FC9A21"
+            }} />
+
+            <FontAwesomeIcon icon={faCircle} size={70} style={{
+                position: 'absolute',
+                bottom: -30,
+                left: 90,
+                color: "#FCCA1C"
+            }} />
+        </>
+    );
+}
+
+export default FondComponent;
