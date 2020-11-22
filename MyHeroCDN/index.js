4ea6bd16d745cea830635b9be0914ef27df55dee
Hoyame Zouhari <56686012+hoyame@users.noreply.github.com>
2020-11-22 17:58:55 +0100

    d

--- MyHeroCDN/index.js
+++ MyHeroCDN/index.js
@@ -1,22 +1,29 @@
 const Express = require('express');
 const multer = require('multer');
 const bodyParser = require('body-parser');
+const fs = require('fs');
 
 const app = Express();
 app.use(bodyParser.json());
 
-var upload = multer({ dest: 'images/' })
+var upload = multer({dest:'uploads/'});
 
+var type = upload.single('recfile');
 
 app.get('/', (req, res) => {
   res.status(200).send('You can post to /api/upload.');
 });
 
-app.post('/api/upload', upload.single('avatar'), (req, res) => {
-  console.log('Zfzifbzuifbiugbibigbigb');
+app.post('/api/upload', type, (req, res) => {
+  var tmp_path = req.file.path;
+  var target_path = 'uploads/' + req.file.originalname;
 
-  console.log('file', req.files);
-  console.log('body', req.body);
+  var src = fs.createReadStream(tmp_path);
+  var dest = fs.createWriteStream(target_path);
+  src.pipe(dest);
+  src.on('end', function() { res.render('complete'); });
+  src.on('error', function(err) { res.render('error'); });
+  
   res.status(200).json({
     message: 'success!',
   });
