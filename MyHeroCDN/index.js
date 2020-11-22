ff264b186d5d160bd87685066cc2c57d48eb53be
Hoyame Zouhari <56686012+hoyame@users.noreply.github.com>
2020-11-22 16:34:32 +0100

    upload

--- MyHeroCDN/index.js
+++ MyHeroCDN/index.js
@@ -4,10 +4,6 @@ const bodyParser = require('body-parser');
 
 const app = Express();
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
-multer({
-  limits: {fieldSize: 25 * 1024 * 1024},
-});
 
 const Storage = multer.diskStorage({
   destination(req, file, callback) {
@@ -18,26 +14,20 @@ const Storage = multer.diskStorage({
   },
 });
 
+const upload = multer({ storage: Storage });
+
 app.get('/', (req, res) => {
-  res.status(200).send('to upload image use this  /api/upload.');
+  res.status(200).send('You can post to /api/upload.');
 });
 
-app.post('/api/upload', (req, res) => {
-  let upload = multer({storage: Storage}).single('picture');
-  upload(req, res, function (err) {
-    if (!req.file) {
-      return res.send('Please select an image to upload');
-    } else if (err instanceof multer.MulterError) {
-      return res.send(err);
-    } else if (err) {
-      return res.send(err);
-    }
-    // Display uploaded image for user validation
-    res.send(req.file.path); // send uploaded image
+app.post('/api/upload', upload.array('photo', 3), (req, res) => {
+  console.log('file', req.files);
+  console.log('body', req.body);
+  res.status(200).json({
+    message: 'success!',
   });
 });
 
 app.listen(3000, () => {
   console.log('App running on http://localhost:3000');
 });
-
