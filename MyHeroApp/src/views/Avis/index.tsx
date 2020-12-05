aaefb57e3256bf439d1c9d6b2405d45c30cf664e
hoyame <zou.hoyame@gmail.com>
2020-12-05 18:06:27 +0100

    app(avis): fix usefull

--- MyHeroApp/src/views/Avis/index.tsx
+++ MyHeroApp/src/views/Avis/index.tsx
@@ -1,13 +1,37 @@
 import { faCircle } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text } from 'react-native';
+import { API_LINK } from '../../App';
 import FondComponent from '../../components/Fond';
 import HeaderComponent from '../../components/Header/header';
 import RateComponent from '../../components/Rate';
+import { useReduxState } from '../../data/store';
 import I18n from '../../i18n/i18n';
 
 const AvisScreen = ({ navigation }) => {
+    const userMail = useReduxState(state => state.user.mail);
+
+    const [description, setDescription] = useState("");
+    const [rate, setRate] = useState(0);
+
+    const sendRate = () => {
+        var params = {
+            source: userMail,
+            description: description,
+            rate: rate
+        }
+    
+        fetch(`${API_LINK}/app/add_rate`, {
+            method: 'POST',
+            headers: {
+              Accept: 'application/json',
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(params),
+        })
+    }
+
     return (
         <>
             <HeaderComponent title={I18n.t("avis")} navigation={navigation} />
@@ -23,7 +47,18 @@ const AvisScreen = ({ navigation }) => {
                     marginBottom: 10
                 }}>MyHeroes</Text>
 
-                <RateComponent title={I18n.t("avisRateApp")} placeholder={I18n.t("avisDescApp")} onClick={() => null} />
+                <RateComponent 
+                    title={I18n.t("avisRateApp")} 
+                    placeholder={I18n.t("avisDescApp")} 
+                    onClick={() => {
+                        sendRate();
+                        navigation.navigate("Home");
+                    }} 
+                    rate={rate}
+                    setRate={setRate}
+                    description={description}
+                    setDescription={setDescription}
+                />
             </View>
         </>
     );
