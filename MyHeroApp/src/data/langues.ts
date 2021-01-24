e5a323bab5c1e761a6726fd807d9ddf4c036a7b4
hoyame <zou.hoyame@gmail.com>
2021-01-24 21:24:07 +0100

    m

--- MyHeroApp/src/data/langues.ts
+++
@@ -1,8 +0,0 @@
-export const Langues = [
-    { name: "Français", id: "fr", img: "https://www.countryflags.io/fr/flat/64.png" },
-    { name: "Italiano", id: "it", img: "https://www.countryflags.io/it/flat/64.png" },
-    { name: "English", id: "gb", img: "https://www.countryflags.io/gb/flat/64.png" },
-    { name: "Español", id: "es", img: "https://www.countryflags.io/es/flat/64.png" },
-    { name: "Deutsche", id: "de", img: "https://www.countryflags.io/de/flat/64.png" },
-    { name: "Português", id: "pt", img: "https://www.countryflags.io/pt/flat/64.png" },
-]
