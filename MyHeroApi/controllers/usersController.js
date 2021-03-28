ddc57345a230a92e8eec6eea060bd675c58a5747
hoyame <zou.hoyame@gmail.com>
2021-03-28 17:47:11 +0200

    n

--- MyHeroApi/controllers/usersController.js
+++ MyHeroApi/controllers/usersController.js
@@ -176,7 +176,7 @@ module.exports.forgotPassword = (req, res, next) => {
 
 		var mailOptions = {
 			from: 'appmyheroes@gmail.com',
-			to: mail,
+			to: email,
 			subject: 'Reset password',
 			html: `Hi there! <br/><br/>
 			Please click on the link below to reset your password:<br/>
