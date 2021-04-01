754bd7ef426dde80d9150cc0f22f2b5e18a9164d
hoyame <zou.hoyame@gmail.com>
2021-04-01 22:59:30 +0200

    add: temp

--- MyHeroAdminApp/App.tsx
+++ MyHeroAdminApp/App.tsx
@@ -1,115 +1,79 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
- *
- * @format
- */
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  useColorScheme,
+  View,
+} from 'react-native';
+import { ReloadInstructions } from 'react-native/Libraries/NewAppScreen';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
+import { faTimesCircle, faUser, faQuestionCircle, faFileAlt } from '@fortawesome/free-regular-svg-icons';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scrollview';
 
- import React from 'react';
- import {
-   SafeAreaView,
-   ScrollView,
-   StatusBar,
-   StyleSheet,
-   Text,
-   useColorScheme,
-   View,
- } from 'react-native';
 
- import {
-   Colors,
-   DebugInstructions,
-   Header,
-   LearnMoreLinks,
-   ReloadInstructions,
- } from 'react-native/Libraries/NewAppScreen';
+const App = () => {
+  const AlertProps = () => {
+    return (
+      <View style={{
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        padding: 15,
+        borderRadius: 15,
+        marginBottom: 10,
+        backgroundColor: '#d80000'
+      }}>
+        <View>  
+          <Text>ID</Text>
+          <Text>Createur</Text>
+          <Text>Description</Text>
+          <Text>Date</Text>
+          <Text>Location</Text>
+        </View>
 
- const Section: React.FC<{
-   title: string;
- }> = ({children, title}) => {
-   const isDarkMode = useColorScheme() === 'dark';
-   return (
-     <View style={styles.sectionContainer}>
-       <Text
-         style={[
-           styles.sectionTitle,
-           {
-             color: isDarkMode ? Colors.white : Colors.black,
-           },
-         ]}>
-         {title}
-       </Text>
-       <Text
-         style={[
-           styles.sectionDescription,
-           {
-             color: isDarkMode ? Colors.light : Colors.dark,
-           },
-         ]}>
-         {children}
-       </Text>
-     </View>
-   );
- };
+        <View>  
+          <TouchableOpacity>
+            <FontAwesomeIcon style={{
+              
+            }} size={28} icon={ faTimesCircle } />
+          </TouchableOpacity>
+        </View>
+      </View>
+    );
+  }
 
- const App = () => {
-   const isDarkMode = useColorScheme() === 'dark';
+  return (
+    <View style={{
+      paddingTop: 60,
+      padding: 35,
+      
+    }}>
 
-   const backgroundStyle = {
-     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-   };
+    <ScrollView>
+      <Text style={{
+        fontSize: 30,
+        fontWeight: '600',
+        marginBottom: 30
+      }}>Alertes</Text>
 
-   return (
-     <SafeAreaView style={backgroundStyle}>
-       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-       <ScrollView
-         contentInsetAdjustmentBehavior="automatic"
-         style={backgroundStyle}>
-         <Header />
-         <View
-           style={{
-             backgroundColor: isDarkMode ? Colors.black : Colors.white,
-           }}>
-           <Section title="Step One">
-             Edit <Text style={styles.highlight}>App.js</Text> to change this
-             screen and then come back to see your edits.
-           </Section>
-           <Section title="See Your Changes">
-             <ReloadInstructions />
-           </Section>
-           <Section title="Debug">
-             <DebugInstructions />
-           </Section>
-           <Section title="Learn More">
-             Read the docs to discover what to do next:
-           </Section>
-           <LearnMoreLinks />
-         </View>
-       </ScrollView>
-     </SafeAreaView>
-   );
- };
+      <AlertProps />
+      <AlertProps />
+      <AlertProps />
+      <AlertProps />
+      <AlertProps />
+      <AlertProps />
+      <AlertProps />
+      <AlertProps />
+      <AlertProps />
+      <AlertProps />
+    </ScrollView>
+      </View>
+  );
+};
 
- const styles = StyleSheet.create({
-   sectionContainer: {
-     marginTop: 32,
-     paddingHorizontal: 24,
-   },
-   sectionTitle: {
-     fontSize: 24,
-     fontWeight: '600',
-   },
-   sectionDescription: {
-     marginTop: 8,
-     fontSize: 18,
-     fontWeight: '400',
-   },
-   highlight: {
-     fontWeight: '700',
-   },
- });
-
- export default App;
+export default App;
