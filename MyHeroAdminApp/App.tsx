92580b6be1bac3d35748ee28f070735197c7db73
hoyame <zou.hoyame@gmail.com>
2021-05-13 14:30:42 +0200

    tweak(adminapp): fix all

--- MyHeroAdminApp/App.tsx
+++ MyHeroAdminApp/App.tsx
@@ -1,19 +1,4 @@
-import React, { useEffect, useState } from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  TouchableHighlight,
-  TouchableOpacity,
-  useColorScheme,
-  View,
-} from 'react-native';
-import { ReloadInstructions } from 'react-native/Libraries/NewAppScreen';
-import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
-import { faTimesCircle, faCheckCircle, faUser, faQuestionCircle, faFileAlt } from '@fortawesome/free-regular-svg-icons';
-import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scrollview';
+import React from 'react';
 import io from 'socket.io-client';
 import axios from 'axios';
 import Page from './Page';
@@ -22,7 +7,7 @@ const SOCKET_URL = 'http://146.59.227.90:3333';
 
 export const socket = io.connect(SOCKET_URL, {
   transports: ['websocket'],
-  reconnectionAttempts: 15 //Nombre de fois qu'il doit réessayer de se connecter
+  reconnectionAttempts: 15, //Nombre de fois qu'il doit réessayer de se connecter
 });
 
 export let d1: React.SetStateAction<never[]> = [];
@@ -30,45 +15,45 @@ export let d2: React.SetStateAction<never[]> = [];
 export let d3: React.SetStateAction<never[]> = [];
 
 export const refresh = () => {
-  axios.get(`${SOCKET_URL}/list/get`)
-    .then((response) => {
-        const data = response.data;
-        console.log("d2", data)
-        d2 = data
+  axios
+    .get(`${SOCKET_URL}/list/get`)
+    .then(response => {
+      const data = response.data;
+      console.log('d2', data);
+      d2 = data;
     })
 
-    .catch((err) => {
-        e(false)
-        console.log("err", err);
-    }
-  )
+    .catch(err => {
+      e(false);
+      console.log('err', err);
+    });
 
-  axios.get(`${SOCKET_URL}/list/get-verif`)
-    .then((response) => {
-        const data = response.data;
-        console.log("d3", data)
-        d3 = data
+  axios
+    .get(`${SOCKET_URL}/list/get-verif`)
+    .then(response => {
+      const data = response.data;
+      console.log('d3', data);
+      d3 = data;
     })
 
-    .catch((err) => {
-        console.log("err", err);
-    }
-  )
-}
+    .catch(err => {
+      console.log('err', err);
+    });
+};
 
 const App = () => {
-  socket.on('connect', function(data: any) {
+  socket.on('connect', function () {
     socket.emit('join', 0);
   });
 
-  socket.on('get_alerts', function(data: any){
-    console.log("get_alerts", data);
+  socket.on('get_alerts', function (data: any) {
+    console.log('get_alerts', data);
     d1 = data;
   });
 
   setInterval(() => {
     refresh();
-  }, 100000)
+  }, 100000);
 
   return (
     <>
--- MyHeroAdminApp/Page.tsx
+++ MyHeroAdminApp/Page.tsx
@@ -1,30 +1,27 @@
-import React, { useEffect, useState } from 'react';
+/* eslint-disable prettier/prettier */
+/* eslint-disable react-native/no-inline-styles */
+import React, {useEffect, useState} from 'react';
 import {
-    RefreshControl,
-  SafeAreaView,
+  RefreshControl,
   ScrollView,
-  StatusBar,
-  StyleSheet,
   Text,
-  TouchableHighlight,
   TouchableOpacity,
-  useColorScheme,
   View,
 } from 'react-native';
-import { ReloadInstructions } from 'react-native/Libraries/NewAppScreen';
-import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
-import { faTimesCircle, faCheckCircle, faUser, faQuestionCircle, faFileAlt } from '@fortawesome/free-regular-svg-icons';
-import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scrollview';
-import io from 'socket.io-client';
-import axios from 'axios';
-import { d1, d2, d3, refresh } from './App';
+import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
+import {
+  faTimesCircle,
+  faCheckCircle,
+} from '@fortawesome/free-regular-svg-icons';
+import {d1, d2, d3, refresh} from './App';
 
+let loading = false;
 
 const Page = () => {
-  const [AlertList, setAlertList] = useState([])
-  const [InformationsH24, setInformationsH24] = useState([])
-  const [InformationsH24AV, setInformationsH24AV] = useState([])
-  
+  const [AlertList, setAlertList] = useState([]);
+  const [InformationsH24, setInformationsH24] = useState([]);
+  const [InformationsH24AV, setInformationsH24AV] = useState([]);
+
   interface IN {
     id: number;
     name: string;
@@ -32,6 +29,7 @@ const Page = () => {
     description: string;
     hour: number;
     city: string;
+    departement: string,
     source: string;
     rate: number;
     level: number;
@@ -41,288 +39,363 @@ const Page = () => {
 
   const AlertProps = (props: IN) => {
     return (
-      <View style={{
-        display: 'flex',
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        padding: 15,
-        borderRadius: 15,
-        marginBottom: 10,
-        backgroundColor: '#d80000'
-      }}>
-        <View>  
+      <View
+        // eslint-disable-next-line react-native/no-inline-styles
+        style={{
+          display: 'flex',
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+          padding: 15,
+          borderRadius: 15,
+          marginBottom: 10,
+          backgroundColor: '#d80000',
+        }}>
+        <View>
           <Text>{props.id}</Text>
-          <Text>{props.name}</Text>
+          <Text>{props.source}</Text>
           <Text>{props.description}</Text>
           <Text>{props.hour}</Text>
-          <Text>{props.city}</Text>
+          <Text>{props.departement} - {props.city}</Text>
         </View>
 
-        <View>  
-          <TouchableOpacity onPress={() => {
+        <View>
+          <TouchableOpacity
+            onPress={() => {
               var params = {
                 identifier: props.identifier,
                 level: props.level,
                 source: props.source,
                 latitude: props.latitude,
                 longitude: props.longitude,
-                description: props.description
-            }
-    
-            let req = {
+                description: props.description,
+              };
+
+              let req = {
                 method: 'POST',
                 headers: {
                   Accept: 'application/json',
                   'Content-Type': 'application/json',
                 },
                 body: JSON.stringify(params),
-            }
-        
-            fetch(`http://146.59.227.90:3333/alerts/remove`, req)
-                .then(function(res) {
-                    console.log(res);
+              };
+
+              fetch('http://146.59.227.90:3333/alerts/remove', req)
+                .then(function (res) {
+                  console.log(res);
                 })
-    
-                .catch(function(err) {
-                    console.log("errror", err)
-                })
-            ;
-          }}>
-            <FontAwesomeIcon size={30} icon={ faTimesCircle } />
+
+                .catch(function (err) {
+                  console.log('errror', err);
+                });
+
+              setTimeout(() => {
+                refresh();
+                setTimeout(() => {
+                  setAlertList(d1);
+                  setInformationsH24(d2);
+                  setInformationsH24AV(d3);
+                  setRefreshing(false);
+                }, 3000);
+              }, 4000);
+            }}>
+            <FontAwesomeIcon size={30} icon={faTimesCircle} />
           </TouchableOpacity>
         </View>
       </View>
     );
-  }
+  };
 
   const NewsProps = (props: IN) => {
     return (
-      <View style={{
-        display: 'flex',
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        padding: 15,
-        borderRadius: 15,
-        marginBottom: 10,
-        backgroundColor: '#366BF8'
-      }}>
-        <View>  
-            <Text>0</Text>
-            <Text>{props.name}</Text>
-            <Text>{props.description}</Text>
-            <Text>{props.hour}</Text>
-            <Text>{props.city}</Text>
+      <View
+        style={{
+          display: 'flex',
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+          padding: 15,
+          borderRadius: 15,
+          marginBottom: 10,
+          backgroundColor: '#366BF8',
+        }}>
+        <View>
+          <Text>0</Text>
+          <Text>{props.name}</Text>
+          <Text>{props.description}</Text>
+          <Text>{props.hour}</Text>
+          <Text>{props.departement} - {props.city}</Text>
         </View>
 
-        <View style={{
-          display: 'flex',
-          flexDirection: 'row'
-        }}>  
-          <TouchableOpacity onPress={() => {
+        <View
+          style={{
+            display: 'flex',
+            flexDirection: 'row',
+          }}>
+          <TouchableOpacity
+            onPress={() => {
               var params = {
                 name: props.name,
                 rate: props.rate,
                 description: props.description,
-                latitude: props.latitude, 
-                longitude: props.longitude
-            }
-    
-            let req = {
+                latitude: props.latitude,
+                longitude: props.longitude,
+                city: props.city,
+                departement: props.departement
+              };
+
+              let req = {
                 method: 'POST',
                 headers: {
                   Accept: 'application/json',
                   'Content-Type': 'application/json',
                 },
                 body: JSON.stringify(params),
-            }
-        
-            fetch(`http://146.59.227.90:3333/list/approvate`, req)
-                .then(function(res) {
-                    console.log(res);
+              };
+
+              fetch('http://146.59.227.90:3333/list/approvate', req)
+                .then(function (res) {
+                  console.log(res);
                 })
-    
-                .catch(function(err) {
-                    console.log("errror", err)
-                })
-            ;
-          }}>
-            <FontAwesomeIcon style={{marginRight: 10}}  size={30} icon={ faCheckCircle } />
+
+                .catch(function (err) {
+                  console.log('errror', err);
+                });
+
+              setTimeout(() => {
+                refresh();
+                setTimeout(() => {
+                  setAlertList(d1);
+                  setInformationsH24(d2);
+                  setInformationsH24AV(d3);
+                  setRefreshing(false);
+                }, 3000);
+              }, 4000);
+            }}>
+            <FontAwesomeIcon
+              style={{marginRight: 10}}
+              size={30}
+              icon={faCheckCircle}
+            />
           </TouchableOpacity>
 
-          <TouchableOpacity onPress={() => {
-                 var params = {
-                    name: props.name,
-                    rate: props.rate,
-                    description: props.description,
-                    latitude: props.latitude, 
-                    longitude: props.longitude
-                }
-        
-                let req = {
-                    method: 'POST',
-                    headers: {
-                      Accept: 'application/json',
-                      'Content-Type': 'application/json',
-                    },
-                    body: JSON.stringify(params),
-                }
-            
-                fetch(`http://146.59.227.90:3333/list/delete`, req)
-                    .then(function(res) {
-                        console.log(res);
-                    })
-        
-                    .catch(function(err) {
-                        console.log("errror", err)
-                    })
-                ;
-          }}>
-            <FontAwesomeIcon size={30} icon={ faTimesCircle } />
+          <TouchableOpacity
+            onPress={() => {
+              var params = {
+                name: props.name,
+                rate: props.rate,
+                description: props.description,
+                latitude: props.latitude,
+                longitude: props.longitude,
+                city: props.city,
+                departement: props.departement
+              };
+
+              let req = {
+                method: 'POST',
+                headers: {
+                  Accept: 'application/json',
+                  'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(params),
+              };
+
+              fetch('http://146.59.227.90:3333/list/delete', req)
+                .then(function (res) {
+                  console.log(res);
+                })
+
+                .catch(function (err) {
+                  console.log('errror', err);
+                });
+
+              setTimeout(() => {
+                refresh();
+                setTimeout(() => {
+                  setAlertList(d1);
+                  setInformationsH24(d2);
+                  setInformationsH24AV(d3);
+                  setRefreshing(false);
+                }, 3000);
+              }, 4000);
+            }}>
+            <FontAwesomeIcon size={30} icon={faTimesCircle} />
           </TouchableOpacity>
         </View>
       </View>
     );
-  }
+  };
 
   const NewsProps2 = (props: IN) => {
     return (
-      <View style={{
-        display: 'flex',
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        padding: 15,
-        borderRadius: 15,
-        marginBottom: 10,
-        backgroundColor: '#138D0B'
-      }}>
-        <View>  
-            <Text>0</Text>
-            <Text>{props.name}</Text>
-            <Text>{props.description}</Text>
-            <Text>{props.hour}</Text>
-            <Text>{props.city}</Text>
+      <View
+        style={{
+          display: 'flex',
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+          padding: 15,
+          borderRadius: 15,
+          marginBottom: 10,
+          backgroundColor: '#138D0B',
+        }}>
+        <View>
+          <Text>0</Text>
+          <Text>{props.name}</Text>
+          <Text>{props.description}</Text>
+          <Text>{props.hour}</Text>
+          <Text>{props.departement} - {props.city}</Text>
         </View>
 
-        <View style={{
-          display: 'flex',
-          flexDirection: 'row'
-        }}>  
-          <TouchableOpacity onPress={() => {
+        <View
+          style={{
+            display: 'flex',
+            flexDirection: 'row',
+          }}>
+          <TouchableOpacity
+            onPress={() => {
               var params = {
                 name: props.name,
                 rate: props.rate,
                 description: props.description,
-                latitude: props.latitude, 
-                longitude: props.longitude
-            }
-    
-            let req = {
+                latitude: props.latitude,
+                longitude: props.longitude,
+                city: props.city,
+                departement: props.departement
+              };
+
+              let req = {
                 method: 'POST',
                 headers: {
                   Accept: 'application/json',
                   'Content-Type': 'application/json',
                 },
                 body: JSON.stringify(params),
-            }
-        
-            fetch(`http://146.59.227.90:3333/list/deleteApprovate`, req)
-                .then(function(res) {
-                    console.log(res);
+              };
+
+              fetch('http://146.59.227.90:3333/list/deleteApprovate', req)
+                .then(function (res) {
+                  console.log(res);
                 })
-    
-                .catch(function(err) {
-                    console.log("errror", err)
-                })
-            ;
-          }}>
-            <FontAwesomeIcon size={30} icon={ faTimesCircle } />
+
+                .catch(function (err) {
+                  console.log('errror', err);
+                });
+
+              setTimeout(() => {
+                refresh();
+                setTimeout(() => {
+                  setAlertList(d1);
+                  setInformationsH24(d2);
+                  setInformationsH24AV(d3);
+                  setRefreshing(false);
+                }, 3000);
+              }, 4000);
+            }}>
+            <FontAwesomeIcon size={30} icon={faTimesCircle} />
           </TouchableOpacity>
         </View>
       </View>
     );
-  }
+  };
+
+  // eslint-disable-next-line no-undef
+  useEffect(() => {
+    if (loading === false) {
+      refresh();
+      setTimeout(() => {
+        setAlertList(d1);
+        setInformationsH24(d2);
+        setInformationsH24AV(d3);
+        setRefreshing(false);
+      }, 3000);
+      loading = true;
+    }
+  });
 
   setTimeout(() => {
     setAlertList(d1);
     setInformationsH24(d2);
     setInformationsH24AV(d3);
-  }, 2000)
+  }, 2000);
 
   const returnAlerts = () => {
     return AlertList.map((v, k) => {
-        return <AlertProps key={k} {...v} />
-    })
-  }   
+      return <AlertProps key={k} {...v} />;
+    });
+  };
 
   const returnNews = () => {
-      return InformationsH24.map((v, k) => {
-          return <NewsProps key={k} {...v} />
-      })
-  }
+    return InformationsH24.map((v, k) => {
+      return <NewsProps key={k} {...v} />;
+    });
+  };
 
   const returnNews2 = () => {
-      return InformationsH24AV.map((v, k) => {
-          return <NewsProps2 key={k} {...v} />
-      })
-  }
+    return InformationsH24AV.map((v, k) => {
+      return <NewsProps2 key={k} {...v} />;
+    });
+  };
 
   const onRefresh = React.useCallback(() => {
     setRefreshing(true);
 
-        refresh();
-        setTimeout(() => {
-            setAlertList(d1);
-            setInformationsH24(d2);
-            setInformationsH24AV(d3);
-            setRefreshing(false);
-        }, 3000)
-    }, []);
+    refresh();
+    setTimeout(() => {
+      setAlertList(d1);
+      setInformationsH24(d2);
+      setInformationsH24AV(d3);
+      setRefreshing(false);
+    }, 3000);
+  }, []);
 
-    const [refreshing, setRefreshing] = React.useState(false);
-
+  const [refreshing, setRefreshing] = React.useState(false);
 
   return (
-    <View style={{
-      paddingTop: 60,
-      padding: 35,
-    }}>
-        <ScrollView
-            refreshControl={
-                <RefreshControl
-                  refreshing={refreshing}
-                  onRefresh={onRefresh}
-                />
-            }
-        >
-            <Text style={{
-                fontSize: 30,
-                fontWeight: '600',
-                marginBottom: 30
-            }}>Alertes</Text>
+    <View
+      style={{
+        paddingTop: 60,
+        padding: 35,
+      }}>
+      <ScrollView
+        refreshControl={
+          <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+        }>
+        <Text
+          style={{
+            fontSize: 30,
+            fontWeight: '600',
+            marginBottom: 30,
+          }}>
+          Alertes
+        </Text>
 
-            {returnAlerts()}
+        {returnAlerts()}
 
-            <Text style={{
-                fontSize: 30,
-                fontWeight: '600',
-                marginTop: 20,
-                marginBottom: 30
-            }}>News a approuver</Text>
+        <Text
+          style={{
+            fontSize: 30,
+            fontWeight: '600',
+            marginTop: 20,
+            marginBottom: 30,
+          }}>
+          News a approuver
+        </Text>
 
-            {returnNews()}
+        {returnNews()}
 
-            <Text style={{
-                fontSize: 30,
-                fontWeight: '600',
-                marginTop: 20,
-                marginBottom: 30
-            }}>News approuvées</Text>
+        <Text
+          style={{
+            fontSize: 30,
+            fontWeight: '600',
+            marginTop: 20,
+            marginBottom: 30,
+          }}>
+          News approuvées
+        </Text>
 
-            {returnNews2()}
-        </ScrollView>
+        {returnNews2()}
+      </ScrollView>
     </View>
   );
 };
