0f9ed4e0ace7fde324424b2314527a35f154c02b
hoyame <zou.hoyame@gmail.com>
2021-05-13 12:20:53 +0200

    fix(firebase): failed to subscribe

--- MyHeroApp/src/App.tsx
+++ MyHeroApp/src/App.tsx
@@ -35,6 +35,26 @@ export const socket = io.connect(SOCKET_URL, {
   reconnectionAttempts: 15 //Nombre de fois qu'il doit réessayer de se connecter
 });
 
+String.prototype.sansAccent = function(){
+    var accent = [
+        /[\300-\306]/g, /[\340-\346]/g, // A, a
+        /[\310-\313]/g, /[\350-\353]/g, // E, e
+        /[\314-\317]/g, /[\354-\357]/g, // I, i
+        /[\322-\330]/g, /[\362-\370]/g, // O, o
+        /[\331-\334]/g, /[\371-\374]/g, // U, u
+        /[\321]/g, /[\361]/g, // N, n
+        /[\307]/g, /[\347]/g, // C, c
+    ];
+    var noaccent = ['A','a','E','e','I','i','O','o','U','u','N','n','C','c'];
+     
+    var str = this;
+    for(var i = 0; i < accent.length; i++){
+        str = str.replace(accent[i], noaccent[i]);
+    }
+     
+    return str;
+}
+
 PushNotification.configure({
   // (optional) Called when Token is generated (iOS and Android)
   onRegister: function (token: any) {
@@ -89,9 +109,6 @@ const _storeData = async (e: string) => {
 }
 
 const App = () => {
-
-
-
   useEffect(() => {
     socket.on('connect', function(data: any) {
       socket.emit('join', 0);
@@ -178,7 +195,9 @@ const App = () => {
     console.log("zufzfizvuibvizvizvzvziuvk")
     firebase.messaging().unsubscribeFromTopic('all')
 
-    MyHeroAlerts.getCityGE(MyHeroService.latitude, MyHeroService.longitude, (e: any) => null, (e: any) => {
+    MyHeroAlerts.getCityGE(MyHeroService.latitude, MyHeroService.longitude, (e: any) => null, (departement: any) => {
+      const e = departement.sansAccent()
+      
       console.log("efiuebgiubegbeigbeirgbrebgiug", e)
       unsubscribeFromTopic();
 
