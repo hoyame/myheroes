cc6d55373fad6a3a0f4106e59e0eb87646550a1d
hoyame <zou.hoyame@gmail.com>
2021-05-14 22:34:29 +0200

    x

--- MyHeroApi/controllers/usersController.js
+++ MyHeroApi/controllers/usersController.js
@@ -361,9 +361,9 @@ module.exports.returnRateUser = (req, res, next) => {
 }
 
 module.exports.addXp = (req, res, next) => {
+	console.log("addXP")
 	const users = req.query.user;
 	const query = `SELECT xp FROM users WHERE pseudo=?`
-	console.log("addXP user", users)
 
 	con.query(query, [users], (err, result, fields) => {
 		let oldXp = result;
